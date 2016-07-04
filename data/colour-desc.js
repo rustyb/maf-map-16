69e110938e79ba391609b6e8d0fed33fb5b59b0c
Colin Broderick <rustybroderick@gmail.com>
2016-07-04 14:03:30 +0200

    countries and colours

---
+++ data/colour-desc.js
@@ -0,0 +1,14 @@
+[
+  {
+    "colour": "1",
+    "desc": "Explain: Base Case"
+  },
+  {
+    "colour": "2",
+    "desc": "Explain: Sensitivity (1)"
+  },
+  {
+    "colour": "3",
+    "desc": "Explain: Sensitivity (2)"
+  }
+]
